80bafa653c83c8efec7948947aba577ed13e4a88
NaritaShosei <onigirikun7171@gmail.com>
2024-09-19 00:47:05 +0900

    エラーが出力されるので要修正

--- Assets/Script/LockOn.cs
+++ Assets/Script/LockOn.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweeng;
+using DG.Tweening;
 
 public class LockOn : MonoBehaviour
 {
-    List<GameObject> _targetPosition = new List<GameObject>();
+    public List<GameObject> _targetPosition = new List<GameObject>();
     SpriteRenderer _sr;
     int _targetCount;
+    bool _isComplete = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +18,9 @@ public class LockOn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow))
+        if ((Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow)) && _isComplete)
         {
+            _isComplete = false;
             _sr.enabled = true;
             var enemy = GameObject.FindGameObjectsWithTag("Enemy");
             foreach (GameObject obj in enemy)
@@ -28,7 +30,8 @@ public class LockOn : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.RightArrow))
             {
                 _targetCount = (_targetCount + 1) % _targetPosition.Count;
-                transform.position = _targetPosition[_targetCount].transform.position;//‚±‚±DOMove‚Å‚â‚è‚½‚¢OnCompreat
+                //transform.position = _targetPosition[_targetCount].transform.position;
+                transform.DOMove(_targetPosition[_targetCount].transform.position, 0.5f).OnComplete(() => _isComplete = true);
             }
             else if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
@@ -37,7 +40,7 @@ public class LockOn : MonoBehaviour
                 {
                     _targetCount = _targetPosition.Count - 1;
                 }
-                transform.position = _targetPosition[_targetCount].transform.position;
+                transform.DOMove(_targetPosition[_targetCount].transform.position, 0.5f).OnComplete(() => _isComplete = true);
             }
         }
 
