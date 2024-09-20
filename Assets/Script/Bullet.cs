85c1ccab87162adde867ae71486d0c2ab29ee97b
NaritaShosei <onigirikun7171@gmail.com>
2024-09-21 00:33:31 +0900

    ロックオンした敵に向かって弾が発射される処理

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -7,11 +7,20 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] float _moveSpeed;
     Rigidbody2D _rb;
+    LockOn _lockOn;
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _rb.velocity =Vector2.up * _moveSpeed;  
+        _lockOn = FindObjectOfType<LockOn>();
+        if (!_lockOn.IsLockOn)
+        {
+            _rb.velocity = Vector2.up * _moveSpeed;
+        }
+        else if (_lockOn.IsLockOn)
+        {
+            _rb.velocity =(_lockOn.transform.position - transform.position).normalized * _moveSpeed;
+        }
     }
 
     // Update is called once per frame
--- Assets/Script/LockOn.cs
+++ Assets/Script/LockOn.cs
@@ -7,13 +7,15 @@ public class LockOn : MonoBehaviour
 {
     public List<GameObject> _targetPosition = new List<GameObject>();
     SpriteRenderer _sr;
-    int _targetCount;
+    public int _targetCount;
     bool _isComplete = true;
     float _distance;
+    public bool IsLockOn;
     // Start is called before the first frame update
     void Start()
     {
         _sr = GetComponent<SpriteRenderer>();
+        IsLockOn = false;
     }
 
     // Update is called once per frame
@@ -21,29 +23,35 @@ public class LockOn : MonoBehaviour
     {
         if ((Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow)) && _isComplete)
         {
-            foreach (var targetPos in _targetPosition)
+            if (_targetPosition.Count != 0)
             {
-                _distance = targetPos.transform.position.x - transform.position.x;
-            }
-            _isComplete = false;
-            _sr.enabled = true;
-            if (Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                _targetCount = (_targetCount + 1) % _targetPosition.Count;
-                //transform.position = _targetPosition[_targetCount].transform.position;
-                transform.DOMove(_targetPosition[_targetCount].transform.position, 0.5f).OnComplete(() => _isComplete = true);
-            }
-            else if (Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                _targetCount--;
-                if (_targetCount < 0)
+                IsLockOn = true;
+                _isComplete = false;
+                _sr.enabled = true;
+                if (Input.GetKeyDown(KeyCode.RightArrow))
                 {
-                    _targetCount = _targetPosition.Count - 1;
+                    _targetCount = (_targetCount + 1) % _targetPosition.Count;
+                    //transform.position = _targetPosition[_targetCount].transform.position;
+                    transform.DOMove(_targetPosition[_targetCount].transform.position, 0.5f).OnComplete(() => _isComplete = true);
                 }
-                transform.DOMove(_targetPosition[_targetCount].transform.position, 0.5f).OnComplete(() => _isComplete = true);
+                else if (Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    _targetCount--;
+                    if (_targetCount < 0)
+                    {
+                        _targetCount = _targetPosition.Count - 1;
+                    }
+                    transform.DOMove(_targetPosition[_targetCount].transform.position, 0.5f).OnComplete(() => _isComplete = true);
+                }
+                Debug.Log("’·‚³" + _targetPosition.Count);
+                Debug.Log("‚¢‚Ü" + _targetCount);
+                Debug.Log("IsLockOn" + IsLockOn);
             }
-            Debug.Log("’·‚³" + _targetPosition.Count);
-            Debug.Log("‚¢‚Ü" + _targetCount);
+        }
+        if (_targetPosition.Count == 0)
+        {
+            IsLockOn = false;
+            _sr.enabled = false;
         }
 
     }
@@ -54,6 +62,11 @@ public class LockOn : MonoBehaviour
     public void Remove(GameObject enemy)
     {
         _targetPosition.Remove(enemy);
+        IsLockOn = false;
+    }
+    private void OnBecameInvisible()
+    {
+        _sr.enabled = false;
     }
 }
 
