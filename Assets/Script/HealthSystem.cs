e23e9697f854bdf3b23cd1824948b63b061c45dc
NaritaShosei <onigirikun7171@gmail.com>
2024-09-22 20:12:20 +0900

    ライフのゲージ

--- Assets/Script/HealthSystem.cs
+++ Assets/Script/HealthSystem.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class HealthSystem : MonoBehaviour
 {
     [SerializeField] float _maxLife;
     float _life;
+    [SerializeField] Image _hpGauge;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,10 +25,12 @@ public class HealthSystem : MonoBehaviour
     }
     public void Life(float currentLife)
     {
+        LifeGauge(currentLife);
         _life += currentLife;
     }
     void LifeGauge(float gauge)
     {
-
+        float currentLife = _life;
+        DOTween.To(() => currentLife / _maxLife, x => _hpGauge.fillAmount = x, (currentLife + gauge) / _maxLife, 0.3f);
     }
 }
